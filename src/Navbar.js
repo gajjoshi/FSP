d8c96b37744e6ded2cda02473912221d4af19e0a
Gaj Joshi <gajbjoshi@gmail.com>
2023-11-09 23:31:56 +0530

    Added my project

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+// Navbar.jsx
+
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="nav-link">Home</span>
+      <span className="nav-link">Contact Us</span>
+      <span className="nav-link">About Us</span>
+      <span className="nav-link">Service</span>
+    </div>
+  );
+};
+
+export default Navbar;
